9648aef82c8989e6581860dd4af8f65e825c9947
MSA <ablanco@msageoconsultores.com>
2024-09-27 16:18:11 -0300

    Refactor user controller and network modules, and add bcrypt for password hashing

--- nodejs-microservicios-redis/api/auth/index.js
+++ nodejs-microservicios-redis/api/auth/index.js
@@ -1,9 +1,49 @@
 const jwt = require('jsonwebtoken')
+const config = require('../../config')
+const secret = config.jwt.secret
 
 function sign(data) {
-  return jwt.sign(data, 'secreto')
+  return jwt.sign(data, secret)
 }
 
+const check = {
+  own: function (req, owner) {
+    const decoded = decodeHeader(req)
+    console.log(decoded)
+
+    // Comprobar si es o no propio
+    if (decoded.id !== owner) {
+      throw new Error('No puedes hacer esto')
+    }
+  }
+}
+
+function verify(token) {
+  return jwt.verify(token, secret)
+}
+
+function getToken(auth) {
+  if (!auth) {
+    throw new Error('No viene token')
+  }
+
+  if (auth.indexOf('Bearer ') === -1) {
+    throw new Error('Formato inválido')
+  }
+
+  let token = auth.replace('Bearer ', '')
+  return token
+}
+
+function decodeHeader(req) {
+  const authorization = req.headers.authorization || ''
+  const token = getToken(authorization)
+  const decoded = verify(token)
+
+  req.user = decoded
+
+  return decoded
+}
 module.exports = {
   sign
 }
